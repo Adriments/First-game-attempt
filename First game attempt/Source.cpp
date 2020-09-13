eaf37c89faba15dcc319aa64c160d549e9bb6cef
Adrian Clements <adriments@gmail.com>
2020-09-13 13:20:32 -0500

    Have viewports working, variable error in vpRight, but rectangle still doesn't appear.

--- First game attempt/Source.cpp
+++ First game attempt/Source.cpp
@@ -12,8 +12,6 @@ int main(int argc, char* args[])
 {
 	SDL_Window* gameWindow = NULL;
 
-	SDL_Surface* background = NULL;
-
 	SDL_Renderer* gameRenderer = NULL;
 
 	SDL_Surface* vpTest = NULL;
@@ -96,8 +94,6 @@ int main(int argc, char* args[])
 		SDL_Rect rectangle1 = { SCREEN_WIDTH * 7/16, SCREEN_HEIGHT * 7/8, SCREEN_WIDTH / 8, SCREEN_HEIGHT / 8 };
 		SDL_SetRenderDrawColor(gameRenderer, 0x00, 0x00, 0x00, 0xFF);
 		SDL_RenderFillRect(gameRenderer, &rectangle1);
-
-		SDL_SetRenderDrawColor(gameRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 		/*
 		vpLeft.x = 0;
 		vpLeft.y = 0;
@@ -106,17 +102,16 @@ int main(int argc, char* args[])
 		SDL_RenderSetViewport(gameRenderer, &vpLeft);
 
 		SDL_RenderCopy(gameRenderer, rendererTest, NULL, NULL);
-		*/
+		
 		vpRight.x = SCREEN_WIDTH * 3 / 4;
 		vpRight.y = 0;
 		vpRight.w = SCREEN_WIDTH / 4;
 		vpRight.h = SCREEN_HEIGHT / 2;
-		SDL_RenderSetViewport(gameRenderer, &vpLeft);
+		SDL_RenderSetViewport(gameRenderer, &vpRight);
 
 		SDL_RenderCopy(gameRenderer, rendererTest, NULL, NULL);
 
-
-
+		*/
 		//Puts everything on screen
 		SDL_RenderPresent(gameRenderer);
 	}
