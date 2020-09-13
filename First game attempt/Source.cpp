fba1ac73fec00ede9500f59925c6ebb571151e64
Adrian Clements <adriments@gmail.com>
2020-09-13 12:51:40 -0500

    Struggling with viewport rendering.

--- First game attempt/Source.cpp
+++ First game attempt/Source.cpp
@@ -16,14 +16,23 @@ int main(int argc, char* args[])
 
 	SDL_Renderer* gameRenderer = NULL;
 
+	SDL_Surface* vpTest = NULL;
+
+	SDL_Texture* rendererTest = NULL;
+
 	bool quit = false;
 
 	SDL_Event e;
+
+	SDL_Rect vpLeft;
+	SDL_Rect vpRight;
 
 	SDL_Init(SDL_INIT_VIDEO);
 
 	gameWindow = SDL_CreateWindow("Fire n' Forget", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	gameRenderer = SDL_CreateRenderer(gameWindow, -1, SDL_RENDERER_ACCELERATED);
+	vpTest = SDL_LoadBMP("default.bmp");
+	rendererTest = SDL_CreateTextureFromSurface(gameRenderer, vpTest);
 
 	if(gameWindow == NULL) 
 	{
@@ -34,6 +43,19 @@ int main(int argc, char* args[])
 	if (gameRenderer == NULL)
 	{
 		printf("Renderer failed to start: %s\n", SDL_GetError() );
+		return 1;
+	}
+
+	if (vpTest == NULL)
+	{
+		printf("IMG failed to load: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	if (rendererTest == NULL)
+	{
+		printf("Could not render texture from surface: %s\n", SDL_GetError());
+		return 1;
 	}
 
 	while (!quit)
@@ -65,7 +87,7 @@ int main(int argc, char* args[])
 				}
 			}
 		}
-
+		
 		//Clears the Renderer
 		SDL_SetRenderDrawColor(gameRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 		SDL_RenderClear(gameRenderer);
@@ -75,10 +97,32 @@ int main(int argc, char* args[])
 		SDL_SetRenderDrawColor(gameRenderer, 0x00, 0x00, 0x00, 0xFF);
 		SDL_RenderFillRect(gameRenderer, &rectangle1);
 
-		//Puts rectangle on screen
-		SDL_RenderPresent(gameRenderer);
+		SDL_SetRenderDrawColor(gameRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+		/*
+		vpLeft.x = 0;
+		vpLeft.y = 0;
+		vpLeft.w = SCREEN_WIDTH / 4;
+		vpLeft.h = SCREEN_HEIGHT / 2;
+		SDL_RenderSetViewport(gameRenderer, &vpLeft);
 
+		SDL_RenderCopy(gameRenderer, rendererTest, NULL, NULL);
+		*/
+		vpRight.x = SCREEN_WIDTH * 3 / 4;
+		vpRight.y = 0;
+		vpRight.w = SCREEN_WIDTH / 4;
+		vpRight.h = SCREEN_HEIGHT / 2;
+		SDL_RenderSetViewport(gameRenderer, &vpLeft);
+
+		SDL_RenderCopy(gameRenderer, rendererTest, NULL, NULL);
+
+
+
+		//Puts everything on screen
+		SDL_RenderPresent(gameRenderer);
 	}
+
+	SDL_DestroyTexture(rendererTest);
+	rendererTest = NULL;
 
 	SDL_DestroyRenderer(gameRenderer);
 	SDL_DestroyWindow(gameWindow);
